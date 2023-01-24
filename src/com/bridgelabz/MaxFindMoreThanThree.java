f2f9089375de92112bebf4e52378faa34912cb33
priyanshusuman9 <priyanshusuman947@gmail.com>
2023-01-25 00:45:07 +0530

    Generics

---
+++ src/com/bridgelabz/MaxFindMoreThanThree.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+public class MaxFindMoreThanThree <T extends Comparable<T>> {  //sorting using comparable interface in collection generic class
+    private T[] inputArray;
+
+    MaxFindMoreThanThree(T[] inputArray){
+        this.inputArray = inputArray;
+    }
+
+    public void  testMaximum() {
+        for(int i=0; i<inputArray.length-1; i++) {
+            if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
+                inputArray[i+1] = inputArray[i];
+            }
+        }
+
+    }
+    private void toPrint() {
+        System.out.println(inputArray[inputArray.length - 1]);
+    }
+    public static void main(String[] args) {
+        Integer[] integer = {15,32,45};
+        String[] string = {"Peach","Banana","Apple"};
+        Float[] floats = {15.1f,32.2f,45.3f};
+        new MaxFindMoreThanThree(integer).testMaximum();
+        new MaxFindMoreThanThree(string).testMaximum();
+        new MaxFindMoreThanThree(floats).testMaximum();
+        new MaxFindMoreThanThree(integer).toPrint();
+        new MaxFindMoreThanThree(string).toPrint();
+        new MaxFindMoreThanThree(floats).toPrint();
+    }
+
+}
